14b0d0a35ca0280f6094fde163c309cbd8fa2141
Jaydeep Sathvara <119073219+jaydeep1124@users.noreply.github.com>
2023-03-13 00:53:42 -0400

    Update signup.java

--- src/electricity/billing/system/signup.java
+++ src/electricity/billing/system/signup.java
@@ -132,4 +132,7 @@ public class signup extends JFrame implements ActionListener {
             new Login();
         }
     }
+            public static void main(String[] args) {
+            new Signup();
+    }
 }
